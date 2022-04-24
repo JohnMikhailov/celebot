d00164fb2c314bd049325adfc7d48ff0cfe60cdf
ivanmikhailov <mikhailovj@gmail.com>
2022-04-24 18:37:59 +0300

    fix models

--- telegram/api/api.go
+++ telegram/api/api.go
@@ -6,11 +6,32 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+
+	"encoding/json"
+	"celebot/telegram/models"
 )
 
 
-func getModel() {
-	
+func getUpdates(url string) telegram.UpdateResponse {
+	res := telegram.UpdateResponse {}
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		json.Unmarshal([]byte(bodyString), &res)
+	}
+
+	return res
 }
 
 
@@ -24,21 +45,8 @@ func LongPolling() {
 			"?timeout=5" +
 			"&limit=1"
 
-		fmt.Print(url)
+		updates := getUpdates(url)
 
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode == http.StatusOK {
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			bodyString := string(bodyBytes)
-			fmt.Printf(bodyString)
-		}
+		fmt.Print(updates.Result[0].MessageInfo.Text)
 	}
 }
--- telegram/models/models.go
+++ telegram/models/models.go
@@ -4,30 +4,42 @@ package telegram
 type Chat struct {
 	// full description https://core.telegram.org/bots/api#chat
 
-	id int `json:"id"`
+	Id int `json:"id"`
 
 	//Type of chat, can be either “private”, “group”, “supergroup” or “channel”
-	type_ string  `json:"type"`
+	Type string  `json:"type"`
 
-	title string  `json:"title"`
-	username string  `json:"username"`
+	Title string  `json:"title"`
+	Username string  `json:"username"`
 
 }
 
 type User struct {
 	// full description https://core.telegram.org/bots/api#user
 
-	id int  `json:"id"`
-	is_bot bool  `json:"is_bot"`
-	first_name string  `json:"first_name"`
-	last_name string  `json:"last_name"`
-	username string  `json:"username"`
+	Id int  `json:"id"`
+	Is_bot bool  `json:"is_bot"`
+	First_name string  `json:"first_name"`
+	Last_name string  `json:"last_name"`
+	Username string  `json:"username"`
 
 }
 
 type Message struct {
-	message_id int  `json:"message_id"`
-	from User  `json:"from"`
-	sender_chat Chat  `json:"sender_chat"`
-	text string  `json:"text"`
+	Message_id int  `json:"message_id"`
+	From User  `json:"from"`
+	Sender_chat Chat  `json:"sender_chat"`
+	Text string  `json:"text"`
+}
+
+
+type Update struct {
+	Update_id int `json:"update_id"`
+	MessageInfo Message `json:"message"`
+}
+
+
+type UpdateResponse struct {
+	Ok bool `json:"ok"`
+	Result []Update `json:"result"`
 }
