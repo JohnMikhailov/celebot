b59d47e9f85044fb46b800b4f997464ad9aadd70
Ivan Mikhailov <meehighlov@Ivans-MacBook-Air.local>
2022-06-13 15:07:14 +0300

    remove dotenv lib

--- app/config.go
+++ app/config.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 type config struct {
@@ -18,19 +16,15 @@ type config struct {
 var config_ config
 
 func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
 	config_.BOTTOKEN = os.Getenv("BOTTOKEN")
-	config_.LONGPOLLING_WORKERS, err = strconv.Atoi(os.Getenv("LONGPOLLING_WORKERS"))
+	longPolingWorkers, err := strconv.Atoi(os.Getenv("LONGPOLLING_WORKERS"))
 	if err != nil {
-		log.Fatalf("Parse .env error for: LONGPOLLING_WORKERS")
+		log.Fatalf("Parse var error for: LONGPOLLING_WORKERS")
 	}
+	config_.LONGPOLLING_WORKERS = longPolingWorkers
 	seconds, err := strconv.Atoi(os.Getenv("DEFAULT_DELAY_BETWEEN_CHECKS_SEC"))
 	if err != nil {
-		log.Fatalf("Parse .env error for: DEFAULT_DELAY_BETWEEN_CHECKS_SEC")
+		log.Fatalf("Parse var error for: DEFAULT_DELAY_BETWEEN_CHECKS_SEC")
 	}
 	config_.DEFAULT_DELAY_BETWEEN_CHECKS_SEC = time.Duration(seconds) * time.Second
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/meehighlov/celebot
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0
-
 require github.com/mattn/go-sqlite3 v1.14.12
